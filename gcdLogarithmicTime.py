073efcfe6884ccd4c3aa2a17b1dad106bb3e34e7
NithinChitra <130959838+NithinChitra@users.noreply.github.com>
2023-11-22 11:09:37 +0530

    update

--- gcdLogarithmicTime.py
+++ gcdLogarithmicTime.py
@@ -1,12 +1,16 @@
 def gcd(a,b):
-    i=2
-    hcf = 0
-    while i<=a:
-        if a%i==0 and b%i==0:
-            if i>hcf:
-                print(hcf)
-                hcf = i
-        i+=1
-    return hcf
+    # i=2
+    # hcf = 0
+    # while i<=a:
+    #     if a%i==0 and b%i==0:
+    #         if i>hcf:
+    #             print(hcf)
+    #             hcf = i
+    #     i+=1
+    # return hcf
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
 res = gcd(10,20)
 print(res)
